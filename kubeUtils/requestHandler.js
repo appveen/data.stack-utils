1cc2501bb854eb3adc53316d614e5b77831a80ba
Jugnu Agrawal <jugnu@appveen.com>
2023-10-25 16:34:41 +0530

    api call fix

--- kubeUtils/requestHandler.js
+++ kubeUtils/requestHandler.js
@@ -25,90 +25,88 @@ const headers = {
 	"Authorization": "Bearer " + dataStack_token
 };
 
-let response = {
-	statusCode: 200,
-	body: null
-};
+// let response = {
+// 	statusCode: 200,
+// 	body: null
+// };
 
 e.get = async (url) => {
 	try {
-		await got.get(`${URL}${url}`, {
+		let response = await got.get(`${URL}${url}`, {
 			headers: headers,
 			https: {
 				rejectUnauthorized: false
 			},
-		}).json()
-			.then(data => response.body = data)
+		});
+		return response;
 	} catch (error) {
 		logger.error(error);
-		response.statusCode = 400;
+		return error;
 	}
-	return response;
 };
 
 e.post = async (url, body) => {
 	try {
-		await got.post(`${URL}${url}`, {
+		let response = await got.post(`${URL}${url}`, {
 			headers: headers,
 			https: {
 				rejectUnauthorized: false
 			},
 			json: body
-		}).json()
-			.then(data => response.body = data)
+		});
+		return response;
 	} catch (error) {
 		logger.error(error);
-		response.statusCode = 400;
+		return error;
 	}
-	return response;
 }
 
 e.patch = async (url, body) => {
 	try {
-		await got.patch(`${URL}${url}`, {
+		let response = await got.patch(`${URL}${url}`, {
 			headers: {
 				"Authorization": "Bearer " + dataStack_token,
 				"Content-Type": "application/merge-patch+json"
 			},
 			json: body
-		}).then(data => response.body = data.body);
+		});
+		return response;
 	} catch (error) {
 		logger.error(error);
-		response.statusCode = 400;
+		return error;
 	}
-	return response;
 }
 
 e.put = async (url, body) => {
 	try {
-		await got.put(`${URL}${url}`, {
+		let response = await got.put(`${URL}${url}`, {
 			headers: headers,
 			https: {
 				rejectUnauthorized: false
 			},
 			json: body
-		}).json().then(data => response.body = data);
+		});
+		return response;
 	} catch (error) {
 		logger.error(error);
-		response.statusCode = 400;
+		return error;
 	}
-	return response.body;
 }
 
 e.delete = async (url, body) => {
 	try {
-		await got.delete(`${URL}${url}`, {
+		let response = await got.delete(`${URL}${url}`, {
 			headers: headers,
 			https: {
 				rejectUnauthorized: false
 			},
 			json: body
-		}).then(data => response.body = data.body);
+		});
+		return response;
 	} catch (error) {
 		logger.error(error);
-		response.statusCode = 400;
+		return error;
 	}
-	return response.body;
 }
 
 module.exports = e;
